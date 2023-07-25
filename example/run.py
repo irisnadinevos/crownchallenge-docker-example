2ab39d2edf42c36e6fc28ef9f7e80002eb3c5e98
Iris Vos <125289748+irisnadinevos@users.noreply.github.com>
2023-07-25 12:59:55 +0200

    Update run.py

--- example/run.py
+++ example/run.py
@@ -1,21 +1,24 @@
 import json
-import random
 from pathlib import Path
+
 
 project_dir = Path(__file__).parent.absolute().parent.absolute()
 input_dir = project_dir / 'input'
 output_dir = project_dir / 'output crownchallenge/TeamName1_task1'
 
+
+def do():
+    files = list(input_dir.glob('*.nii.gz'))
+
+    for f in files:
+        save_json(f.stem / 'result_Lippert.json')
+
+
 def save_json(filename):
 
     data = {'Anterior class': 'g', 'Posterior class': 'h'}
-
+    
     with open(output_dir / filename, mode='w', encoding ='utf8') as json_file:
        json.dump(data, json_file, indent=4)
            
     return
-
-files = list(input_dir.glob('*.nii.gz'))
-
-for f in files:
-    save_json(f.stem / 'result_Lippert.json')
